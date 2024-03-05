20c629849312b1aa0d3ecd7cfd539334c55aa1b1
Marko Stojanovic <stojanovic1994@yahoo.com>
2024-03-05 11:49:24 +0100

    Update repo in deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Creating Docker Container from Data-App repo..."
-sudo docker build -f Dockerfile -t rdlabengpa/ids_be_data_app:develop .
+echo "Creating Docker Container from Data-App SFTP repo..."
+sudo docker build -f Dockerfile -t rdlabengpa/ids_be_data_app_sftp:develop .
 cd ..
-echo "Data-App is ready"
+echo "Data-App SFTP is ready"
 echo "Starting deployment to Docker Hub"
 sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-sudo docker push rdlabengpa/ids_be_data_app:develop
+sudo docker push rdlabengpa/ids_be_data_app_sftp:develop
 echo "Data-App deployed to Docker Hub"
